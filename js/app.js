3f912820c68da0c403caa0f6fee575381c55e848
Toan Truong <toan.truongvantt@gmail.com>
2021-01-09 18:09:16 +0700

    finish menu section

--- js/app.js
+++ js/app.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
     dropRadius: 25,
     perturbance: 0.03,
   });
+  // magnific poppup
+  $('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+      enabled:true
+    }
+    // other options
+  });
 });
